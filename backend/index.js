5598fcfcc2fdbc5665e1a8d52144a14a9e594909
Brandon Cifuentes <bsride02@gmail.com>
2026-04-04 08:47:45 -0500

    feat: Se crea servidor basico en backend/index.js/

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,17 @@
 //Importing the database connection
+import express from 'express'
 import client from './src/database/connection.js'
+
+const app = express() 
+
 await client.connect() 
 console.log('Conexión exitosa a PostgreSQL')
+
+app.get('/health', (req, res) => {
+  res.send('Health ok')
+})
+app.listen(3000)
+
+
+
+
